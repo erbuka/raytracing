be7c90426d285fcb44439b93f9d9cc3ae700e006
erbuka <erbuka@hotmail.com>
2018-12-24 10:28:18 +0100

    vs2017

--- premake5.lua
+++ premake5.lua
@@ -2,7 +2,9 @@ workspace "Raytracer"
     configurations { "Debug", "Release" }
     architecture "x86_64"
     startproject "Sandbox0"
-    system "Windows"
+    
+    filter "system:windows"
+        systemversion "latest"
 
     filter "configurations:Debug"
         defines { "DEBUG" }
@@ -15,33 +17,37 @@ workspace "Raytracer"
 project "Raytracer"
     kind "StaticLib"
     language "C++"
-    targetdir "bin/%{cfg.buildcfg}"
-    
+    location "Raytracer"
 
-    files { "Raytracer/**.h", "Raytracer/**.cpp" }
+    targetdir "bin/%{cfg.buildcfg}/%{prj.name}"
+    objdir "bin-int/%{cfg.buildcfg}/%{prj.name}"
+
+    files { "%{prj.name}/**.h", "%{prj.name}/**.cpp" }
 
 project "Sandbox0"
     kind "ConsoleApp"
     language "C++"
-    targetdir "build/%{cfg.buildcfg}"
-    debugdir "build/%{cfg.buildcfg}"
+    location "Sandbox0"
+    
+    targetdir "bin/%{cfg.buildcfg}/%{prj.name}"
+    objdir "bin-int/%{cfg.buildcfg}/%{prj.name}"
 
-    files { "Sandbox0/**.h", "Sandbox0/**.cpp" }
+    files { "%{prj.name}/**.h", "%{prj.name}/**.cpp" }
 
     includedirs { "Raytracer", "vendor/SFML/include" }
+
+    defines { "SFML_STATIC" }
 
     libdirs { "vendor/SFML/lib" }
 
     links { "Raytracer", "freetype", "opengl32", "winmm", "gdi32" }
 
-    defines { "SFML_STATIC" }
-
-    postbuildcommands {
-        "{COPY} \"Sandbox0/res\" \"%{cfg.buildtarget.directory}res\""
-    }
 
     filter "configurations:Debug"
         links { "sfml-system-s-d", "sfml-window-s-d", "sfml-graphics-s-d" }
 
     filter "configurations:Release"
         links { "sfml-system-s", "sfml-window-s", "sfml-graphics-s" }  
+        postbuildcommands {
+            "{COPY} res/ ../bin/%{cfg.buildcfg}/%{prj.name}/res"
+        }        
